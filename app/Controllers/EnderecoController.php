67a062233a51a1400fcf4464f0b62a4b6705911b
FelipeOropeza <felipe2006.co@gmail.com>
2026-02-13 16:19:30 -0300

    feat: refatorar busca de CEP para usar o serviço de curl do CodeIgniter

--- app/Controllers/EnderecoController.php
+++ app/Controllers/EnderecoController.php
@@ -8,14 +8,21 @@ class EnderecoController extends ResourceController
 {
     public function buscarCep($cep)
     {
-        $url = "https://viacep.com.br/ws/{$cep}/json/";
+        $cep = preg_replace('/\D/', '', $cep);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
+        if (strlen($cep) !== 8) {
+            return $this->response
+                ->setStatusCode(400)
+                ->setJSON(['erro' => 'CEP inválido']);
+        }
 
-        return $this->response->setJSON(json_decode($response));
+        $client = \Config\Services::curlrequest();
+
+        $response = $client->get("https://viacep.com.br/ws/{$cep}/json/");
+
+        return $this->response->setJSON(
+            json_decode($response->getBody(), true)
+        );
     }
+
 }
